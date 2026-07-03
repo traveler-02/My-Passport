a60917645a6ca3ac16f28cda2a08a826592c74e0
traveler-02 <rhonnie.traveler2@gmail.com>
2026-07-03 14:05:02 -0500

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAVqpblplMLjPSt2vSXCCN-5hbXZFPW2vU",
+  authDomain: "my-passport-211af.firebaseapp.com",
+  projectId: "my-passport-211af",
+  storageBucket: "my-passport-211af.firebasestorage.app",
+  messagingSenderId: "118053564604",
+  appId: "1:118053564604:web:5fe465dd4bc681fcd7e0b9"
+};
